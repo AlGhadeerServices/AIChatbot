2277068014a9bab348306c3caa88f7f805f4514b
AlGhadeerServices <info@alghadeerservices.com>
2023-06-01 18:47:30 +0400

    latest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a98d04d27c09afd7"; }
+/******/ 		__webpack_require__.h = function() { return "c67040795b6c3f9b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
